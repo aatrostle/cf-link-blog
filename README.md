8abf1dcabc058a002152d559c81c4ee2cbdd65e5
Aaron Trostle <aaron.trostle@gmail.com>
2013-05-09 23:36:19 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+## Link Blog
+
+**An app a day keeps the skills sharp!** A simple Rails app to keep track of helpful links you come across.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkBlog::Application.config.secret_token = '3119abba9fe14e85e779e582bfcc8c5831e149638239798e6109e7afeb068feabb78b3408d9e873986d7094536fc707a75a5b4a162ce660f768bbd84df372de0'
